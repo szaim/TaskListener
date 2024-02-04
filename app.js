fb0029c5a83e324e5b1e89db61e0e41995fd0d5a
szaim <zaimsofia81@gmail.com>
2024-02-04 12:52:39 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    // Your JavaScript and jQuery code goes here
+  
+    // Example event listeners:
+    $('#add-task').click(function () {
+      // Add new task logic
+    });
+  
+    $('#task-list').on('click', '.delete-task', function () {
+      // Delete task logic
+    });
+  
+    $('#task-list').on('change', '.complete-task', function () {
+      // Mark as complete logic
+    });
+  });
+  
